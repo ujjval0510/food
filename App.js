218a302283dcd3347ad2818efeda5b592f9674fe
ujjvalVebuin <ujjvalshrimali@thinkbiz.co.in>
2020-08-20 14:42:47 +0900

    remove unnecessary code

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen';
